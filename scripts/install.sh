8c869a8fd0a1904bdacd0dfb99a1e93608a80cfa
Cyrill Burgener <cyrill.burgener@gmail.com>
2025-10-08 13:44:29 +0200

    Write known_hosts file

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,30 +10,39 @@ BASE_DIR="${2:-/usr/local}"
 if [ "$CI" = "true" ]; then
     echo "Running in CI mode: Starting SSH Agent and loading key from environment"
 
+    if [ -z "$SSH_KNOWN_HOSTS" ]; then
+        echo "ERROR: CI mode requires SSH_KNOWN_HOSTS environment variable." >&2
+        exit 1
+    fi
+
+    # Write known_hosts to file
+    mkdir -p ./.ssh && echo "$SSH_KNOWN_HOSTS" > "./.ssh/known_hosts"
+    SSH_OPTS="-o StrictHostKeyChecking=yes -o UserKnownHostsFile=./.ssh/known_hosts"
+
     # Start the agent and set environment variables in the current script's shell
     eval "$(ssh-agent -s)" > /dev/null
 
     # Kill the agent when the script exits. $SSH_AGENT_PID is set by 'eval' above
     trap "echo 'Stopping SSH Agent'; kill $SSH_AGENT_PID;" EXIT
 
-    if [ -z "$HOST_SSH_KEY" ]; then
-        echo "ERROR: CI mode requires HOST_SSH_KEY environment variable." >&2
+    if [ -z "$SSH_HOST_KEY" ]; then
+        echo "ERROR: CI mode requires SSH_HOST_KEY environment variable." >&2
         exit 1
     fi
 
     # Add key from environment variable, removing carriage returns
-    echo "$HOST_SSH_KEY" | tr -d '\r' | ssh-add -
+    echo "$SSH_HOST_KEY" | ssh-add -
 else
     echo "Running in local mode: Using SSH key from ~/.ssh"
 fi
 
 echo "Copying files..."
-rsync -avz "./build/ci-server" "${REMOTE_HOST}:${BASE_DIR}/bin/ci-server"
-rsync -avz "./migrations" "./ui" "${REMOTE_HOST}:${BASE_DIR}/share/ci-server/"
+rsync -e "ssh ${SSH_OPTS}" -avz "./build/ci-server" "${REMOTE_HOST}:${BASE_DIR}/bin/ci-server"
+rsync -e "ssh ${SSH_OPTS}" -avz "./migrations" "./ui" "${REMOTE_HOST}:${BASE_DIR}/share/ci-server/"
 echo ""
 
 echo "Restarting service..."
-ssh -t "${REMOTE_HOST}" "systemctl restart ci.service"
+ssh ${SSH_OPTS} -t "${REMOTE_HOST}" "systemctl restart ci.service"
 echo ""
 
 echo "Done"
